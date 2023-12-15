aa972f4ae12842f2b0856fbb8ece1b75e000a684
darmyn <81786165+darmyn@users.noreply.github.com>
2023-12-15 00:31:08 -0500

    Update config.php

--- php/scripts/config.php
+++ php/scripts/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', '172.31.22.43');
-define('DB_USER', 'Davyn200489002');
-define('DB_PASSWORD', 'bIEIgrEZmq');
-define('DB_NAME', 'Davyn200489002');
+define('DB_HOST', 'REDACTED');
+define('DB_USER', 'REDACTED');
+define('DB_PASSWORD', 'REDACTED');
+define('DB_NAME', 'REDACTED');
 ?>
